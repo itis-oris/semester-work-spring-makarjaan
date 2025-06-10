ffcd80723823c92aa195cf5f89cc9bda0c8e1570
makarjaan <arina.makarova.08@inbox.ru>
2025-06-10 16:27:10 +0300

    fix: error with the database

--- src/main/java/com/makarova/entity/ApartmentPhoto.java
+++ src/main/java/com/makarova/entity/ApartmentPhoto.java
@@ -2,6 +2,7 @@ package com.makarova.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "apartment_photos")
@@ -20,7 +21,7 @@ public class ApartmentPhoto {
     private String photoUrl;
 
     @Column(name = "created_at", columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
-    private String createdAt;
+    private LocalDateTime createdAt;
 
     @Column(name = "type_of_advert", nullable = false)
     private String typeOfAdvert;
